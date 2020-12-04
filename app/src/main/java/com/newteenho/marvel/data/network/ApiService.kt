74c8ca19dd1534bff4dc16db62fa221ea4c75edc
n3w733n <niltinhosupriano@gmail.com>
2020-12-04 12:13:16 -0300

    Network finalizada.

--- app/src/main/java/com/newteenho/marvel/data/network/ApiService.kt
+++ app/src/main/java/com/newteenho/marvel/data/network/ApiService.kt
@@ -2,14 +2,16 @@ package com.newteenho.marvel.data.network
 
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
-import retrofit2.create
+
 
 object ApiService {
-    private fun initRetrofit(): Retrofit{
+
+    private fun initRetrofit(): Retrofit {
         return Retrofit.Builder()
             .baseUrl("https://gateway.marvel.com/v1/public/")
             .addConverterFactory(MoshiConverterFactory.create())
             .build()
     }
+
     val service: Endpoint = initRetrofit().create(Endpoint::class.java)
 }
